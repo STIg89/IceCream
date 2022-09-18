d9b9a7562006c6239935659c70978d26dc025cf2
STIg89 <107355932+STIg89@users.noreply.github.com>
2022-09-18 14:35:33 +0000

    Deploying to gh-pages from @ STIg89/command-project-HTML-CSS@833fcbffed78c9ca76124cb1760a39abfac0fc57 🚀

--- index.a3f06bf1.js
+++
@@ -1,2 +0,0 @@
-var swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"}});
-//# sourceMappingURL=index.a3f06bf1.js.map
