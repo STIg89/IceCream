57198056a6b738e83c9dbd795f930947935bac61
STIg89 <107355932+STIg89@users.noreply.github.com>
2022-09-18 00:37:21 +0000

    Deploying to gh-pages from @ STIg89/command-project-HTML-CSS@3d8c7464239d2c29af9f2b25c7c10d1b0f445a5e 🚀

---
+++ index.24507b06.js
@@ -0,0 +1,2 @@
+(()=>{const o={openModalBtn:document.querySelector("[location-data-modal-open]"),closeModalBtn:document.querySelector("[location-data-modal-close]"),modal:document.querySelector("[location-data-modal]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)})();
+//# sourceMappingURL=index.24507b06.js.map
