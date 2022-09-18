9b18010a19a4c024c10c41cf202c5e3daca4f40c
STIg89 <107355932+STIg89@users.noreply.github.com>
2022-09-18 08:55:48 +0000

    Deploying to gh-pages from @ STIg89/command-project-HTML-CSS@322fa26c174c548046ce294440e67b7d2bfe2d3d 🚀

--- index.24507b06.js
+++
@@ -1,2 +0,0 @@
-(()=>{const o={openModalBtn:document.querySelector("[location-data-modal-open]"),closeModalBtn:document.querySelector("[location-data-modal-close]"),modal:document.querySelector("[location-data-modal]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)})();
-//# sourceMappingURL=index.24507b06.js.map
--- index.7ad93ce4.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[location-data-modal-open]"),closeModalBtn:document.querySelector("[location-data-modal-close]"),modal:document.querySelector("[location-data-modal]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.7ad93ce4.js.map
