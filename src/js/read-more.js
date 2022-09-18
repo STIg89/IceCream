d9b813877972d7860da03da49df39faa8e9c24d3
Mantrova <annamantrova5@gmail.com>
2022-09-19 01:18:40 +0300

    about read more and modal

---
+++ src/js/read-more.js
@@ -0,0 +1,15 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('[read-button-open]'),
+    closeMenuBtn: document.querySelector('[read-button-close]'),
+    menu: document.querySelector('[read-mobile]'),
+  };
+
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+  function toggleMenu() {
+    document.body.classList.toggle("modal-open");
+    refs.menu.classList.toggle("is-hidden");
+  }
+})();
